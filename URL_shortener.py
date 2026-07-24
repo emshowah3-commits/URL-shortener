3320930a93653b8a29dd70c689057d223a52ab98
Enk-sho portfolio <emshowah3@gmail.com>
2026-07-23 22:23:04 -0300

    Add files via upload

--- URL_shortener.py
+++ URL_shortener.py
@@ -1,12 +1,19 @@
 import pyshorteners
 
-# Users input of url
+
 URL = input("Input a URL: ")
 
-# shortner
-shortener = pyshorteners.Shortener()
+print("1. Short url")
+print("2. Long url")
+print("3. clean the Url")
 
-# Shortner the users url
+option = int(input("Choose a url choose: "))
 
-short_URL = shortener.tinyurl.short(URL)
-print(short_URL)
+if option[1]:
+    shortener = pyshorteners.Shortener()
+    short_URL = shortener.tinyurl.short(URL)
+    print("\n")
+    print(short_URL)
+
+elif option[2]:
+    expanded_url = sl.tinyurl.expand(URL)
